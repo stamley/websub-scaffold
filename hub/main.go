6f01931ceb2eedac58a9b01bf036c21a3d9123e1
Axel <axellystam@gmail.com>
2024-05-27 22:10:35 +0200

    start of case

--- hub/main.go
+++ hub/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello Hub, implement me")
+	http.HandleFunc("/subscribe", handleSubscribe)
+	http.HandleFunc("/publish", handlePublish)
+
+	http.ListenAndServe(":8080", nil)
+
+	fmt.Println("Started server")
+}
+
+func handleSubscribe(w http.ResponseWriter, r *http.Request) {
+	// Handle sub registration
+	// Verify intent
+	// Respond correctly
+
+	if r.Method != "POST" {
+		http.Error(w, "This method is not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	/*
+			if err := r.ParseForm(); err != nil {
+		        http.Error(w, "Invalid form data", http.StatusBadRequest)
+		        return
+		    }
+	*/
+
+	mode := r.FormValue("hub.mode")
+	topic := r.FormValue("hub.topic")
+	callback := r.FormValue("hub.callback")
+
+	if mode == "subscribe" {
+
+	} else if mode == "unsubscribe" {
+
+	} else {
+		http.Error(w, "Invalid mode", http.StatusBadRequest)
+	}
+
+}
+
+func handlePublish(w http.ResponseWriter, r *http.Request) {
+	// Generate JSON data
+	// Sign message
+	// Post to all subscribers
+}
+
+// Use this signature to create X-hub signature header for outgoing
+// post requests to subscriber
+func generateSignature(data, secret string) string {
+	hmac := hmac.New(sha256.New, []byte(secret))
+	hmac.Write([]byte(data))
+	return hex.EncodeToString(hmac.Sum(nil))
 }
